72df47661310a159c72c9340eef8bcfb3397580a
Matias Quevedo <matiasquevedo.sabbath@gmail.com>
2019-10-13 18:15:49 -0300

    markets schema

--- app/Notification.php
+++ app/Notification.php
@@ -9,7 +9,7 @@ class Notification extends Model
     //
     protected $table = "notifications";
 
-    protected $fillable = ['id','title','body','topic','number'];
+    protected $fillable = ['id','title','body','topic','user_id'];
 
     public function user(){
     	return $this->belongsTo('App\User');
--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'id','name', 'email', 'password',
     ];
 
     /**
@@ -27,7 +27,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function notificaciones(){
+    public function notifications(){
         return $this->hasMany('App\Notification');
     }
 
